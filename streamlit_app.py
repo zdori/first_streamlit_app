3c7d999ea5c1ee8c6cd30d3fd83a3bdacdcbac75
zdori <125553488+zdori@users.noreply.github.com>
2023-02-16 13:57:41 +0100

    Update streamlit_app.py
    added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 
 streamlit.title("Healthy Diner")
 streamlit.header("Breakfast Menu")
-streamlit.text("🥑🍞menu item 1")
+streamlit.text("🥑 menu item 1")
 streamlit.text("🐔 menu item 2")
 streamlit.text("🥗 menu item 3")
+
+streamlit.header("Build your own fruit smoothie")
