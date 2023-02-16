c63479307f7ed63f0d3bfad1462bdcaa187f8ac7
zdori <125553488+zdori@users.noreply.github.com>
2023-02-16 14:01:31 +0100

    Update streamlit_app.py
    added pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("Healthy Diner")
 streamlit.header("Breakfast Menu")
@@ -7,3 +8,7 @@ streamlit.text("🐔 menu item 2")
 streamlit.text("🥗 menu item 3")
 
 streamlit.header("Build your own fruit smoothie")
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
