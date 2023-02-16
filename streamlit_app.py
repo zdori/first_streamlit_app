e4801df351d7c4e7d6b0989295379c49a5a947df
zdori <125553488+zdori@users.noreply.github.com>
2023-02-16 13:56:15 +0100

    Update streamlit_app.py
    added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("🥑🍞menu item 1")
+streamlit.text("🐔 menu item 2")
+streamlit.text("🥗 menu item 3")
