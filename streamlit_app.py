2a728ecf4b01bd3049ad953f8f27ce0e6d787748
zdori <125553488+zdori@users.noreply.github.com>
2023-02-20 14:51:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,10 +22,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information!")
   else:
@@ -36,11 +35,6 @@ except URLError as e:
   streamlit.error()
     
 streamlit.write('The user entered ', fruit_choice)
-
-
-# write your own comment -what does the next line do? 
-# write your own comment - what does this do?
-
 
 streamlit.stop()
 
